4b39e1d8a3a6d0486306eeb1e5b01069cf533731
timHa91 <tim.andre.hartmann@gmail.com>
2023-09-18 19:29:15 +0200

    adding base entity. closes #10

--- pet-clinic-data/src/main/java/tim/springframework/mypetclinic/model/Person.java
+++ pet-clinic-data/src/main/java/tim/springframework/mypetclinic/model/Person.java
@@ -1,6 +1,6 @@
 package tim.springframework.mypetclinic.model;
 
-public class Person {
+public class Person extends BaseEntity{
 
     private String firstName;
     private String lastName;
--- pet-clinic-data/src/main/java/tim/springframework/mypetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/tim/springframework/mypetclinic/model/PetType.java
@@ -1,6 +1,6 @@
 package tim.springframework.mypetclinic.model;
 
-public class PetType {
+public class PetType extends BaseEntity{
 
     private String name;
 
